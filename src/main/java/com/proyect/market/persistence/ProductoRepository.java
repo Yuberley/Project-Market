a20b4314ce7b0cd2da3c5aa5292055f698a3045d
Yuberley <yuberleyguerrero1999@gmail.com>
2021-05-04 23:58:05 -0500

    README

--- src/main/java/com/proyect/market/persistence/ProductoRepository.java
+++ src/main/java/com/proyect/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.proyect.market.persistence;
 
 import com.proyect.market.persistence.crud.ProductoCrudRepository;
 import com.proyect.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,6 +21,21 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    // Consult a specific producto with id
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    // Save a product with id
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    // Delete a product with id
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 
 }
